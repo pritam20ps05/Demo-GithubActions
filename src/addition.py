7526e3d44fc74f4eb5a718ee031ce7b23b1a439e
Pritam Das <49360491+pritam20ps05@users.noreply.github.com>
2025-02-07 20:59:47 +0530

    Add build workflow

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,11 @@
 # app.py
-# This is a test commit
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+if __name__=='__main__':
+    a, b = map(int, input('Enter two numbers: ').split(' '))
+    print(f'{a} + {b} = { add(a, b) }')
